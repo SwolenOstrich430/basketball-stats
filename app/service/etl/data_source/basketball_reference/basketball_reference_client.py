1a6dc462760b85adf4b7a7f103075932fbd8b855
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-11 14:11:22 -0600

    adding skeleton for basketball reference client

---
+++ app/service/etl/data_source/basketball_reference/basketball_reference_client.py
@@ -0,0 +1,14 @@
+'''
+Wrapper for basketball_reference_client library. 
+Used initially to pull raw data for: 
+    * teams 
+    * rosters 
+    * players 
+    * game-by-game stats
+'''
+class BasketballReferenceClient():
+
+    def __init__(self):
+        pass
+
+    
