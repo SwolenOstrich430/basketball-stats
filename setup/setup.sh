61e6aaace341dac6eeb9f29df6424c7418eaab32
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-11 09:39:46 -0600

    adding errors if python and pip fail to install

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,6 +8,28 @@ if command -v python3 &> /dev/null; then
     echo "Python 3 is installed."
 else
     echo "Python not installed...installing now"
-    brew install python 
+    brew install python
 fi
 
+if ! command -v python3 &> /dev/null
+then
+    echo "Error: pip is not installed or not found in your PATH."
+    echo "Please install pip or ensure it's added to your system's PATH."
+    exit 1 # Exit with an error code
+fi
+
+echo "Checking if pip Installed" 
+if command -v pipx &> /dev/null; then
+    echo "pip is installed."
+else
+    echo "pip is not installed. Attempting to install it..."
+    # You can add installation commands here, e.g.:
+    brew install pipx
+fi
+
+if ! command -v pipx &> /dev/null
+then
+    echo "Error: pip is not installed or not found in your PATH."
+    echo "Please install pip or ensure it's added to your system's PATH."
+    exit 1 # Exit with an error code
+fi
