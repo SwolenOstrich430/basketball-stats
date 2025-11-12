4ed8dad17ecfb89c58c9f30f49bb1ed5a0413e28
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-12 07:35:00 -0600

    adding baseline for pytest to run in vscode finally

---
+++ app/service/etl/bball_reference/bball_reference_client.py
@@ -0,0 +1,7 @@
+
+class BballReferenceClient():
+    def __init__(self):
+        pass 
+
+    def get_teams(self):
+        return 1
---
+++ tests/service/etl/bball_reference/test_bball_reference_client.py
@@ -0,0 +1,5 @@
+import pytest
+
+class TestBballReferenceClient():
+    def test_get_teams(self):
+        assert 1 == 1
