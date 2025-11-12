834e15fb3bf3f53447394889687b24eea8cd7794
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-12 07:47:23 -0600

    adding basic unit test for get_teams

--- tests/service/etl/bball_reference/test_bball_reference_client.py
+++ tests/service/etl/bball_reference/test_bball_reference_client.py
@@ -1,5 +1,13 @@
 import pytest
 
+from app.dto.etl.bball_reference.team_dto import TeamDto
+from app.service.etl.bball_reference.bball_reference_client import BballReferenceClient
+
 class TestBballReferenceClient():
-    def test_get_teams(self):
-        assert 1 == 1
+    def test_get_teams_returns_a_list_of_bball_ref_team_dto(self):
+        client = BballReferenceClient()
+        teams = client.get_teams()
+
+        assert (len(teams) > 0)
+        for team in teams:
+            assert isinstance(type(team), type(TeamDto))
