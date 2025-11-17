8522c4185935e87b6a666fc0099b3bd7a459a7c9
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-17 08:13:57 -0600

    adding generic storage provider interface to storage provider class

--- app/service/storage/storage_provider.py
+++ app/service/storage/storage_provider.py
@@ -1,3 +1,18 @@
-class StorageProvider():
+from app.service.storage.igeneric_storage_provider import IGenericStorageProvider
+from app.service.storage.istorage_provider import IStorageProvider
+
+class StorageProvider(IGenericStorageProvider):
     def __init__(self):
         pass
+
+    def get_provider(self) -> IStorageProvider:
+        return super().get_provider()
+    
+    def upload_file(bucket_name: str, file_name: str) -> bool:
+        return super().upload_file(file_name)
+    
+    def download_file(bucket_name: str, file_name: str) -> str:
+        return super().download_file(file_name)
+    
+    def file_exists(bucket_name: str, file_name: str) -> bool:
+        return super().file_exists(file_name)
