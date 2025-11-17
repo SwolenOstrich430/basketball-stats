17e6f82a977900ba6a5c6355f292a14968b75c8f
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-17 11:40:01 -0600

    fixing private _get_config method in config_provider test suite

--- tests/service/config/test_config_provider.py
+++ tests/service/config/test_config_provider.py
@@ -21,7 +21,7 @@ class TestConfigProvider():
     def test_get_searches_for_all_provided_keys_in_current_app_config(self, mocker):
         mocker.patch.object(
             self.subject, 
-            'get_config', 
+            '_get_config', 
             return_value=self.config
         )
 
@@ -36,7 +36,7 @@ class TestConfigProvider():
     def test_get_throws_key_error_if_provided_key_does_not_exist(self, mocker):
         mocker.patch.object(
             self.subject, 
-            'get_config', 
+            '_get_config', 
             return_value=self.config
         )
 
