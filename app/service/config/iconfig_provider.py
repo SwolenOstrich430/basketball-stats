552a5385dcc5cbfd9c34eaefc5fdba61a06faf29
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-17 11:19:07 -0600

    adding private _get_config method

--- app/service/config/iconfig_provider.py
+++ app/service/config/iconfig_provider.py
@@ -3,5 +3,9 @@ from abc import ABC, abstractmethod
 class IConfigProvider(ABC):
 
     @abstractmethod
-    def get(*keys: str) -> str:
+    def get(self, *keys: str) -> str:
         pass 
+
+    @abstractmethod
+    def _get_config(self) -> dict:
+        pass
